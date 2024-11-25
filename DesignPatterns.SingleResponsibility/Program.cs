6163207d5f3fb82901e14b39fa541fc33aead373
Jesper Raundahl <zacban@gmail.com>
2024-11-25 22:14:50 +0100

    Single Responsibility Examples

---
+++ DesignPatterns.SingleResponsibility/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Net;
+
+namespace DesignPatterns.SingleResponsibility;
+
+public class Journal {
+    private readonly List<string> entries = new List<string>();
+    private static int count = 0;
+
+    public int AddEntry(string text) {
+        entries.Add($"{++count}: {text}");
+        return count;
+    }
+
+    public void RemoveEntry(int index) {
+        entries.RemoveAt(index);
+    }
+
+    public override string ToString() {
+        return string.Join(Environment.NewLine, entries);
+    }
+}
+
+public class Persistence {
+    public void SaveToFile(Journal journal, string filename, bool overwrite = false) {
+        if (overwrite || !File.Exists(filename)) {
+            File.WriteAllText(filename, journal.ToString());
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var j = new Journal();
+        j.AddEntry("I cried today.");
+        j.AddEntry("I ate a bug.");
+        Console.WriteLine(j);
+
+        if (!Directory.Exists("temp"))
+            Directory.CreateDirectory("temp");
+
+        var p = new Persistence();
+        var filename = @"temp\journal.txt";
+        p.SaveToFile(j, filename, true);
+
+        Process.Start("notepad.exe", filename);
+    }
+}
