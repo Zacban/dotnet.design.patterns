21ae3dc710b21d3065e00a8b9a77096ecfbc3c9a
Jesper Raundahl <zacban@gmail.com>
2024-11-25 22:31:34 +0100

    OpenClosed Filtering from size and color

--- DesignPatterns.OpenClosedPrinciple/Program.cs
+++ DesignPatterns.OpenClosedPrinciple/Program.cs
@@ -39,6 +39,15 @@ public class ProductFilter
                 yield return product;
         }
     }
+
+    public static IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+    {
+        foreach (var product in products)
+        {
+            if (product.Size == size && product.Color == color)
+                yield return product;
+        }
+    }
 }
 
 class Program
