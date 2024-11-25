c1433cb99287d2ac17f1f52eab3c41d460523b4d
Jesper Raundahl <zacban@gmail.com>
2024-11-25 22:29:25 +0100

    OpenClosed Basic Filtering

--- DesignPatterns.OpenClosedPrinciple/Program.cs
+++ DesignPatterns.OpenClosedPrinciple/Program.cs
@@ -1,9 +1,60 @@
-﻿namespace DesignPatterns.OpenClosedPrinciple;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
+
+namespace DesignPatterns.OpenClosedPrinciple;
+
+public enum Color { Red, Green, Blue }
+public enum Size { Small, Medium, Large, Yuge }
+
+public class Product
+{
+    public string Name;
+    public Color Color;
+    public Size Size;
+
+    public Product(string name, Color color, Size size)
+    {
+        Name = name;
+        Color = color;
+        Size = size;
+    }
+}
+
+public class ProductFilter
+{
+    public static IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+    {
+        foreach (var product in products)
+        {
+            if (product.Color == color)
+                yield return product;
+        }
+    }
+
+    public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+    {
+        foreach (var product in products)
+        {
+            if (product.Size == size)
+                yield return product;
+        }
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var apple = new Product("Apple", Color.Green, Size.Small);
+        var tree = new Product("Tree", Color.Green, Size.Large);
+        var house = new Product("House", Color.Blue, Size.Large);
+
+        Product[] products = { apple, tree, house };
+
+        Console.WriteLine("Green products (old):");
+        foreach(var product in ProductFilter.FilterByColor(products, Color.Green))
+        {
+            Console.WriteLine($" - {product.Name} is green");
+        }
     }
 }
