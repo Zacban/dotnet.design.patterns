8018fc06706c2a5476ba769b7f8c1138f4a009da
Jesper Raundahl <zacban@gmail.com>
2024-11-27 23:07:17 +0100

    Basic Example without any patterns - Rectangle

--- DesignPatterns.LiskovSubstitution/Program.cs
+++ DesignPatterns.LiskovSubstitution/Program.cs
@@ -1,9 +1,31 @@
 ﻿namespace DesignPatterns.LiskovSubstitution;
 
+public class Rectangle {
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int width, int height) {
+        Width = width;
+        Height = height;
+    }
+
+    public override string ToString() {
+        return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+    }
+}
+
 class Program
 {
+    static public int Area(Rectangle r) => r.Width * r.Height;
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Rectangle rc = new Rectangle(2,3);
+
+        Console.WriteLine($"{rc} has area {Area(rc)}");
+
     }
 }
