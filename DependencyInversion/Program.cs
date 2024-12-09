146a2e98b254ee411a8ff6971890918ac0e2ddfb
Jesper Raundahl <zacban@gmail.com>
2024-12-09 22:11:17 +0100

    Add Builder Project and update readme

--- DependencyInversion/Program.cs
+++ DependencyInversion/Program.cs
@@ -47,18 +47,6 @@ public interface IRelationshipBrowser
 
 public class Research
 {
-    // public Research(Relationships relationships)
-    // {
-    //     var relations = relationships.Relations;
-    //     foreach (var r in relations.Where(
-    //         x => x.Item1.Name == "John" &&
-    //         x.Item2 == Relationship.Parent
-    //     ))
-    //     {
-    //         Console.WriteLine($"John has a child called {r.Item3.Name}");
-    //     }
-    // }
-
     public Research(IRelationshipBrowser browser)
     {
         foreach (var p in browser.FindAllChildrenOf("John"))
---
+++ DesignPatterns.Builder/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns.Builder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
