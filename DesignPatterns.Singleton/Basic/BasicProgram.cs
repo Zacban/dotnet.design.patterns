eeaa524bfcdca2abf7b1312b9c9056846ccfdb31
Jesper Raundahl <zacban@gmail.com>
2024-12-30 14:15:35 +0100

    Implement Singleton pattern with SingletonDatabase class and IDatabase interface

--- DesignPatterns.Singleton/Basic/BasicProgram.cs
+++ DesignPatterns.Singleton/Basic/BasicProgram.cs
@@ -1,7 +1,41 @@
+using System.Linq;
+
 namespace DesignPatterns.Singleton.Basic;
-    public static class BasicProgram
+public static class BasicProgram
+{
+    public static void Run(string[] args)
     {
-        public static void Run(string[] args) {
-            Console.WriteLine("Hello, BasicProgram!");
-        }
+        Console.WriteLine("Hello, BasicProgram!");
     }
+}
+
+public interface IDatabase {
+    int GetPopulation(string name);
+}
+
+public class SingletonDatabase : IDatabase
+{
+    private Dictionary<string, int> capitals;
+
+    // private SingletonDatabase()
+    // {
+    //     Console.WriteLine("Initializing database");
+
+    //     capitals = File.ReadAllLines("capitals.txt")
+    //         .Batch(2)
+    //         .ToDictionary(
+    //             list => list.ElementAt(0).Trim(),
+    //             list => int.Parse(list.ElementAt(1))
+    //         );
+    // }
+
+    public int GetPopulation(string name)
+    {
+        return capitals[name];
+    }
+
+    private static readonly Lazy<SingletonDatabase> instance =
+        new(() => new SingletonDatabase());
+
+    public static SingletonDatabase Instance => instance.Value;
+}
