99e880014bad26cc01ce5496588a178b916bd157
Jesper Raundahl <zacban@gmail.com>
2024-12-31 11:35:29 +0100

    Add Monostate pattern implementation with CEO class and update Main method

--- DesignPatterns.Monostate/Program.cs
+++ DesignPatterns.Monostate/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var ceo = new CEO();
+        ceo.Name = "Adam Smith";
+        ceo.Age = 55;
+
+        var ceo2 = new CEO();
+        Console.WriteLine(ceo2);
     }
 }
